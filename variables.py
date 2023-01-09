ccfe1f23fc37082fb0bc56ad5a070b2556d701e7
Nishad <abdulnishad12@gmail.com>
2023-01-09 23:40:04 +0530

    variables

--- variables.py
+++ variables.py
@@ -2,8 +2,8 @@ def variables():
     integer_variable = 10
     string_variable = 'hello python'
     float_vaariable = '0.3'
-    complex_varible = 2+4j
-    complex_varible_2 = complex(1,2)
+    complex_varible = 2 + 4j
+    complex_varible_2 = complex(1, 2)
 
     print(integer_variable, type(integer_variable))
     print((string_variable, type(string_variable)))
@@ -11,12 +11,13 @@ def variables():
     print(complex_varible, type(complex_varible))
     print(complex_varible_2, type(complex_varible_2))
 
-#     different way of variable assignments
-    a=b=c=100
-    print(a,b,c)
-    x,y,z = 'a','b','c'
-    print(x,y,z)
-    print(x*3)
+    #     different way of variable assignments
+    a = b = c = 100
+    print(a, b, c)
+    x, y, z = 'a', 'b', 'c'
+    print(x, y, z)
+    print(x * 3)
+
 
 if __name__ == '__main__':
     variables()
